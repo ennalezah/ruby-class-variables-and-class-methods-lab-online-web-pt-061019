c1085980b38cbaff23194c5112b7cd6e47c60426
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-07-03 05:50:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,8 +33,8 @@ class Song
   
   def self.genre_count
     @@genres.each do |genre|
-      @@genre_count[genre] += 1 
-      @@genre_count[genre] ||= 1
+      # @@genre_count[genre] += 1 
+      @@genre_count[genre] ||+= 1
       
     end
     @@genre_count
